34436886c128094e55a169bb184a34b608422140
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2021-10-23 16:18:16 -0300

    landing page update 3.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portifólio</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Sobre mim</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Entre em contato</a></li>
                     </ul>
